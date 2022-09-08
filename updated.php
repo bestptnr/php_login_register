bbdbbdf4f9b0403af0bea359504b7b940c2c23fc
Phuthanet Roopsoong <73695646+bestptnr@users.noreply.github.com>
2022-09-08 12:12:51 +0700

    Update updated.php

--- updated.php
+++ updated.php
@@ -1,10 +1,10 @@
 
 <?php
     if(isset($_POST['username']) && isset($_POST['std_id'])){
-        $host = "us-cdbr-east-05.cleardb.net";
-        $user = "b86f1f666b16da";
-        $password = "60c89eb7";
-        $database = "heroku_8321f7eb84ba79a";
+        $host = "";
+        $user = "a";
+        $password = ";
+        $database = "";
         $conn = mysqli_connect($host,$user,$password,$database);
         $conn->set_charset("utf8");
         $std_id = $_POST['std_id'];
