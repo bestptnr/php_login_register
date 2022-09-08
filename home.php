8f29e5168842157ef03e79809153dc1a4f7fc97c
Phuthanet Roopsoong <73695646+bestptnr@users.noreply.github.com>
2022-09-08 12:13:24 +0700

    Update home.php

--- home.php
+++ home.php
@@ -4,10 +4,10 @@
         header('location:index.php');
     }
         
-    $host = "us-cdbr-east-05.cleardb.net";
-    $user = "b86f1f666b16da";
-    $password = "60c89eb7";
-    $database = "heroku_8321f7eb84ba79a";
+  $host = "";
+    $user = "a";
+    $password = ";
+    $database = "";
     $conn = mysqli_connect($host,$user,$password,$database);
     $conn->set_charset("utf8");
     $user = $_SESSION['username'];
